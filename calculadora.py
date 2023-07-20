d09585ce0f4c3115e11830f1aa6caf3ac8207287
PAULO ROBERTO MOLINA BASAGLIA <betobasaglia@gmail.com>
2023-07-19 22:01:17 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+def soma(a, b):
+	print("A soma é: ", a+b)
+
+def subtrai(a, b):
+	print("A diferença é: ", a-b)
+
+def multiplica(a, b):
+	print("A produto é: ", a*b)
+
+def divide(a, b):
+	# testar se o b é diferente de zero
+	print("A divisão é: ", a/b)
+
+#Programa principal
+
+print("Calculadora simples")
+
+num1 = float(input("Insira o primeiro numero: "))
+num2 = float(input("Insira o segundo numero: "))
+
+soma(num1, num2)
+subtrai(num1, num2)
+multiplica(num1, num2)
+divide(num1, num2)
